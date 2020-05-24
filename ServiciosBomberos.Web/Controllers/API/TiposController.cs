58d218b7297ad808539bfb14606947f647c51276
Bughycop <64602223+Bughycop@users.noreply.github.com>
2020-05-24 21:01:47 +0200

    API TiposController

---
+++ ServiciosBomberos.Web/Controllers/API/TiposController.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiciosBomberos.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ServiciosBomberos.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class TiposController : Controller
+    {
+        private readonly ITipoRepository tipoRepository;
+
+        public TiposController(ITipoRepository tipoRepository)
+        {
+            this.tipoRepository = tipoRepository;
+        }
+
+        #region Metodos
+        [HttpGet]
+        public IActionResult GetTipos()
+        {
+            return Ok(this.tipoRepository.GetAll());
+        }
+
+        #endregion
+    }
+}
